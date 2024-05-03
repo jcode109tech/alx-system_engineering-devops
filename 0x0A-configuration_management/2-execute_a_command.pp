e08d9b3ab77e2be58137eeb58b49bf20315b38b6
jcode109tech <jedydiahr9@gmail.com>
2024-05-03 18:40:34 +0300

    0x0A-configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,6 +2,4 @@
 
 exec { 'pkill -f killmenow':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
-  refreshonly => true,                  # Only execute when triggered explicitly
-  logoutput   => true,                  # Log the output of the command
 }
